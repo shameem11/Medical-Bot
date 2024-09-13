7d9cf690abd3df4b6998f7140f1a2bf2763a9fe2
Mohammed Shameem <62173066+shameem11@users.noreply.github.com>
2024-09-13 18:31:47 +0400

    setup file add

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages,setup
+
+
+setup(
+    name= "Medical-Bot",
+    version= '0.0.1',
+    author='shameem',
+    author_email='Shameemmon.mk@email.com',
+    packages= find_packages(),
+    install_requires =[]
+)
