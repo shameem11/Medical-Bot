951bbc6c3f0b83a0f67c48a1d196e780c64d78fe
Mohammed Shameem <62173066+shameem11@users.noreply.github.com>
2024-09-13 17:28:35 +0400

    file template updated

---
+++ template.py
@@ -0,0 +1,33 @@
+import os 
+from pathlib import Path
+import logging
+
+logging.basicConfig(level=logging.INFO,format="[%(asctime)s]:%(message)s:")
+
+list_of_files =[
+    "src/__init__.py",
+    "src/prompt.py",
+    "setup.py",
+    "research/training.ipynb",
+    "streamlit.py",
+    "store_index.py"
+
+]
+
+for file_path in list_of_files:
+    file_path=Path(file_path)
+    filedir,filename = os.path.split(file_path)
+
+    if filedir !="":
+        os.makedirs(file_path)
+        logging.info(f"creating directory;{filedir}for the file {filename}")
+
+
+    if (not os.path.exists(file_path)):
+        with open(file_path,'w') as f:
+            pass 
+        logging.info(f"creating empty file {file_path}")
+    else:
+        logging.info(f"{filename} is already created")
+
+
