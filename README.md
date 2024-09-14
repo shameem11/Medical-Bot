502fca8e7ba99a0ef9a0c46f31846a4e1315d4d9
Mohammed Shameem <62173066+shameem11@users.noreply.github.com>
2024-09-14 11:40:50 +0400

    Loader updated

--- README.md
+++ README.md
@@ -1,8 +1,15 @@
 # Medical-Bot
-
+#1 step 
 # create a evn 
->conda create -n MedicalBot python=3.9 -y
+>conda create -n Medical-Bot python=3.9 -y
 
 conda activate MedicalBot
 
-``` conda activate MedicalBot ```
+>conda activate MedicalBot 
+
+## step 2 install Requirements.txt
+
+>pip install -r Requirements.txt
+
+## Download the model
+>https://huggingface.co/TheBloke/Llama-2-7B-Chat-GGML/blob/main/llama-2-7b-chat.ggmlv3.q3_K_S.bin
---
+++ store_index.py
@@ -0,0 +1,15 @@
+import os 
+from src.loader import load_pdf,text_split_chunks,download_hugging_face_embeddings,store_in_faiss_vector_db
+from langchain.vectorstores import FAISS
+
+
+pdf_directory = "C:/Users/shame/OneDrive/Documents/GitHub/Medical-Bot/Data/"  
+pdf_documents = load_pdf(pdf_directory)
+
+
+pdf_chunks = text_split_chunks(pdf_documents)
+
+embedding_model = download_hugging_face_embeddings()
+
+index_path="FAISS_index"
+vector_store = store_in_faiss_vector_db(pdf_chunks,embedding_model,index_path)
